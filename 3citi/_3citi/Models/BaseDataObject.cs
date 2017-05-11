b4c8c5e5bc3fa06bbdc515c6f61021af5d0a43cb
uiopak <44174807+uiopak@users.noreply.github.com>
2017-05-11 20:33:16 +0200

    Add project files.

---
+++ 3citi/_3citi/Models/BaseDataObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using _3citi.Helpers;
+
+namespace _3citi.Models
+{
+    public class BaseDataObject : ObservableObject
+    {
+        public BaseDataObject()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        /// <summary>
+        /// Id for item
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Azure created at time stamp
+        /// </summary>
+        public DateTimeOffset CreatedAt { get; set; }
+
+        /// <summary>
+        /// Azure UpdateAt timestamp for online/offline sync
+        /// </summary>
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        /// <summary>
+        /// Azure version for online/offline sync
+        /// </summary>
+        public string AzureVersion { get; set; }
+    }
+}
---
+++ 3citi/_3citi/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace _3citi.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
